8fe3dc16ec42c3e63380219e7601269580367c6f
Jihun <wlgjs06061@naver.com>
2024-07-08 16:19:51 +0900

    2024-07-08

--- class1/10951A+B-4.py
+++ class1/10951A+B-4.py
@@ -1,23 +1,14 @@
 import sys
 sum_list = []
 
-# for i in sys.stdin:
-#     if i.strip() == "":
-#         break
-
-#     a, b = map(int, i.split())
-#     sum_list.append(a+b)
-
-# for result in sum_list:
-#     print(result)
-
-
-for i in input():
+for i in sys.stdin:
     if i.strip() == "":
         break
-    
+
     a, b = map(int, i.split())
     sum_list.append(a+b)
 
 for result in sum_list:
     print(result)
+
+
