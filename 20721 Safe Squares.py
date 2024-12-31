67710b08d760b090ab530349e6a52da4998efc04
Jihun <wlgjs06061@naver.com>
2024-12-31 11:51:10 +0900

    2024-12-31

---
+++ 20721 Safe Squares.py
@@ -0,0 +1,57 @@
+location = []
+R_location = []
+R_col_location = []
+R_row_location = []
+
+for _ in range(8):
+    row = input()
+    location.append(list(row))
+
+for i in range(8):
+    for j in range(8):
+        if(location[i][j] == 'R'):
+            R_location.append((i, j))
+
+for row,col in R_location:
+    R_row_location.append(row)
+    R_col_location.append(col)
+
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if i not in R_row_location and j not in R_col_location:
+            cnt += 1
+
+
+# cnt = 0
+# for i in range(8):
+#     for j in range(8):
+#         if(location[i][j] == '.'):
+#             cnt += 1
+
+print(cnt)
+            
+        
+# # 개선된 코드
+# # 입력받기
+# board = [input() for _ in range(8)]
+
+# # 공격받는 행과 열 추적
+# attacked_rows = set()
+# attacked_cols = set()
+
+# # R의 위치 기록
+# for i in range(8):
+#     for j in range(8):
+#         if board[i][j] == 'R':
+#             attacked_rows.add(i)
+#             attacked_cols.add(j)
+
+# # 안전한 칸 계산
+# safe_count = 0
+# for i in range(8):
+#     for j in range(8):
+#         if i not in attacked_rows and j not in attacked_cols:
+#             safe_count += 1
+
+# print(safe_count)
--- 4118 Fred's Lotto Tickets.py
+++ 4118 Fred's Lotto Tickets.py
@@ -1,14 +1,16 @@
 lotto = set()
-user_num = set()
 
 for i in range(1, 50):
     lotto.add(i)
 
 while(True):
+    
     n = int(input())
+    
     if(n == 0):
         break
 
+    user_num = set()
 
     for i in range(n):
         user_lotto = input()
@@ -19,12 +21,41 @@ while(True):
     else:
         print('No')
 
+    
+
+    
+    
+
+
+
+
+
+# 개선된 코드
+ # 상수: 1부터 49까지의 숫자 집합
+LOTTO_NUMBERS = set(range(1, 50))
+
+# 결과를 저장할 리스트
+results = []
+
+while True:
+    # 티켓 개수 입력받기
+    n = int(input())
+    if n == 0:
+        break
+
+    # 모든 티켓에서 등장한 숫자를 저장할 집합
     user_num = set()
 
-    
-    
+    # 티켓 입력 처리
+    for _ in range(n):
+        ticket = map(int, input().split())
+        user_num.update(ticket)
 
+    # 결과 확인
+    if LOTTO_NUMBERS == user_num:
+        results.append("Yes")
+    else:
+        results.append("No")
 
-
-
-
+# 최종 결과 출력
+print("\n".join(results))
