043d27ca4758771803718e1d118d1ad179c7d5c9
jhkr1 <110290060+jhkr1@users.noreply.github.com>
2025-08-18 15:13:54 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = ''
+    for i in range(len(myString)):
+        if myString[i] == "a" or myString[i] == "A" :
+            answer +=  myString[i].upper()
+        else:
+            answer += myString[i].lower()
+        
+    return answer
